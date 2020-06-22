97838d82971d890d44513bd1b531354e663e77f8
Daniel Scroggins <61557261+dscrog@users.noreply.github.com>
2020-06-22 14:57:01 -0400

    update package version

--- source/templates/image.js
+++ source/templates/image.js
@@ -1,3 +1,9 @@
 import { template } from '../../references/quantum.js';
 
-export const image = template('', '');
+const html = `
+`;
+
+const css = `
+`;
+
+export const image = template(html, css);
